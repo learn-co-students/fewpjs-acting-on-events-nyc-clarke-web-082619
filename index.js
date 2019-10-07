d835f819c57703b670410f20875fdddcafd49de8
github username <github email address>
2019-10-07 10:52:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+
+//let dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    let dodger = document.getElementById("dodger");
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+    console.log(dodger.style.left)
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    let dodger = document.getElementById("dodger");
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+    console.log(dodger.style.left);
+    dodger.style.left = `${left + 1}px`;
+}
+
+
+
+
+
+document.addEventListener("keydown", function(e) {
+    const key = e.key
+    if (key === "ArrowLeft") {
+        //console.log(e);
+        moveDodgerLeft();
+    }
+});
+
+document.addEventListener("keydown", function(e){
+    const key = e.key
+    if (key === "ArrowRight"){
+        //console.log(e);
+        moveDodgerRight();
+    }
+});
